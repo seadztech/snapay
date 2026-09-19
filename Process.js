1148ad10ba7c277074aa810bca7ea7d741f6876c
Sydney kipchirchir <sydney.kipchirchir@BIMASKENYA.local>
2026-09-19 19:32:23 +0300

    updated the process to use 1270001

--- Process.js
+++ Process.js
@@ -7,7 +7,11 @@ const { Worker } = require("bullmq");
 
 const IORedis = require("ioredis");
 
-const connection = new IORedis({ maxRetriesPerRequest: null });
+const connection = new IORedis({ 
+  host: "127.0.0.1",
+  port: 6379,
+  maxRetriesPerRequest: null 
+});
 
 const smsQueue = require("./Queue");
 
